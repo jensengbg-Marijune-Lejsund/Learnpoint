b68f03572689a49dfc6450ee2d8b7ebd00a37a47
Marijune-Lejsund <59868177+jensengbg-Marijune-Lejsund@users.noreply.github.com>
2022-05-27 11:35:22 +0200

    Add files via upload

---
+++ BlogPage.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="blog">
+    <div class="nav">
+      <NavBar></NavBar>
+    </div>
+    <div class="title">Blog</div>
+    <div class="blog__wrapper">
+      <div class="blog__card">
+        <img class="blogImage" v-bind:src="code" />
+        <div class="bloginfo">
+          <p>May 1, 2022</p>
+          <h1>Coding is the Future</h1>
+          <p class="post">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+            inventore aperiam nesciunt nisi ullam dicta, placeat vero
+            quibusdam incidunt sunt ipsa numquam officia error voluptate
+            alias. Veniam laborum sequi omnis?
+          </p>
+          <a href="blogpost.html">
+            <button class="blog__button">Read More</button>
+          </a>
+        </div>
+      </div>
+      <div class="blog__card">
+        <img class="blogImage" v-bind:src="meeting" />
+        <div class="bloginfo">
+          <p>April 20, 2022</p>
+          <h1>Meeting online now?</h1>
+          <p class="post">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+            inventore aperiam nesciunt nisi ullam dicta, placeat vero
+            quibusdam incidunt sunt ipsa numquam officia error voluptate
+            alias. Veniam laborum sequi omnis?
+          </p>
+          <a href="blogpost2.html">
+            <button class="blog__button">Read More</button>
+          </a>
+        </div>
+      </div>
+      <div class="blog__card">
+        <img class="blogImage" v-bind:src="social" />
+        <div class="bloginfo">
+          <p>April 03, 2022</p>
+          <h1>Social Media Today</h1>
+          <p class="post">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+            inventore aperiam nesciunt nisi ullam dicta, placeat vero
+            quibusdam incidunt sunt ipsa numquam officia error voluptate
+            alias. Veniam laborum sequi omnis?
+          </p>
+          <a href="blogpost3.html">
+            <button class="blog__button">Read More</button>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="more">
+      <button>More Blog Posts</button>
+    </div>
+    <div class="footer">
+      <Footer></Footer>
+    </div>
+  </div>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue';
+import Footer from './components/Footer.vue';
+import code from '../assets/code.avif';
+import meeting from '../assets/meeting.avif';
+import grading from '../assets/social.avif';
+
+export default {
+  data: function() {
+    return {
+      logoImage: logoImage,
+      code: code,
+      meeting: meeting,
+      grading: grading,
+    };
+  },
+  components: {
+    NavBar,
+    Footer,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ Footer.vue
@@ -0,0 +1,71 @@
+<template>
+  <footer>
+    <div class="footer__wrapper">
+      <div class="footer__card">
+        <h2>Learnpoint</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea culpa itaque reiciendis, ratione a temporibus odio ipsum cupiditate perferendis, explicabo neque, veniam optio tempora aspernatur obcaecati recusandae quas ipsa tempore.</p>
+      </div>
+      <div class="footer__card">
+        <h2>Contacts</h2>
+        <p>012 3456 789</p>
+        <p>Box 7</p>
+        <p>Stockholm</p>
+        <p>learnpoint@lp.se</p>
+      </div>
+      <div class="footer__card">
+        <h2>Links</h2>
+        <a class="foot__links" href="home.html">Home</a>
+        <a class="foot__links" href="team.html">Our Team</a>
+        <a class="foot__links" href="contact.html">Contact</a>
+        <a class="foot__links" href="pricing.html">Pricing</a>
+        <a class="foot__links" href="index.html">Blog</a>
+      </div>
+      <div class="footer__card">
+        <h2>Connect with Us</h2>
+        <img class="foot__connect" src=".assets/connect.png" alt />
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+.footer__wrapper {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr;
+  background-color: #0975ff;
+  color: white;
+  padding: 8px;
+}
+
+.footer__card h2 {
+  font-family: 'Nunito', sans-serif;
+  font-weight: 450;
+  font-size: 30px;
+}
+
+.footer__card {
+  margin: 10px;
+  display: flex;
+  flex-direction: column;
+}
+
+.footer__card p {
+  font-size: 15px;
+  display: flex;
+}
+
+.foot__links {
+  text-decoration: none;
+  font-family: 'Raleway', sans-serif;
+  color: white;
+  cursor: pointer;
+  margin-left: 10px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
+
+.foot__connect {
+  margin-left: -20px;
+}
+</style>
